aefd67f514f077d1f2eb6b7ec2396fb4b98dd059
Sanches-Rafael <myself_sanchesrafael@outlook.com>
2023-03-16 16:41:06 -0300

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
